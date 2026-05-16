73e2bad4780a92b336c725142e14f5be6d4a0073
buitruong16102004-cmd <buitruong16102004@gmail.com>
2026-05-17 00:18:22 +0700

    Create ThongKe.cs

---
+++ QlyNhaHang/Models/Models/ThongKe.cs
@@ -0,0 +1,9 @@
+namespace QlyNhaHang.Models
+{
+    public class ThongKe
+    {
+        public string TenMon { get; set; }
+        public int SoLuongBan { get; set; }
+        public decimal DoanhThu { get; set; }
+    }
+}
